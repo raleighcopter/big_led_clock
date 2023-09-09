6846ab869437b473b19536f9f8c0b7a9ec8e5b01
raleighcopter <23077474+raleighcopter@users.noreply.github.com>
2023-09-09 12:02:08 -0400

    Update ask_transmitter_si7021.ino

--- ask_transmitter_si7021.ino
+++ ask_transmitter_si7021.ino
@@ -13,9 +13,6 @@
 Adafruit_Si7021 sensor = Adafruit_Si7021();
 
 RH_ASK driver(2000, 0, 2, 0);
-// RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
-// RH_ASK driver(2000, 3, 4, 0); // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
-// RH_ASK driver(2000, PD14, PD13, 0); STM32F4 Discovery: see tx and rx on Orange and Red LEDS
 
 int transmitter_code = 152; // must match receiver code
 
